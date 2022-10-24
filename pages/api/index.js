89b07b02a120d327c3543292a9a7cd9a3ca63ccd
geckoxys <edward.andre.23@gmail.com>
2022-10-24 10:57:49 -0400

    verify header2

--- pages/api/index.js
+++ pages/api/index.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
        optionsSuccessStatus: 200, 
     });
 
-    name=[...name, {header:req.header, body:req.body}]
+    name=[...name, {header:req.headers, body:req.body}]
     
     res.status(200).json(name)
 }
