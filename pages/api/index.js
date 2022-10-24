b7d194f2b6b0026528de64a0e5390acdd2dcfc2e
geckoxys <edward.andre.23@gmail.com>
2022-10-24 10:46:26 -0400

    again

--- pages/api/index.js
+++ pages/api/index.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
        optionsSuccessStatus: 200, 
     });
 
-    name=[...name,req]
+    name=[...name, req.body]
     
     res.status(200).json(name)
 }
