73dba7e2965dcf925299c39f8ebfa4592f83ba98
geckoxys <edward.andre.23@gmail.com>
2022-10-24 11:17:52 -0400

    post verify3

--- pages/api/index.js
+++ pages/api/index.js
@@ -12,9 +12,9 @@ export default async function handler(req, res) {
     });
 
     if(req.method !== 'POST') res.status(200).json(name)
-    if(req.body.projectId !== "prj_Uneqt4CLqAh6YprTsAUfBqhfuHLs") return
+    // if(req.body.projectId !== "prj_Uneqt4CLqAh6YprTsAUfBqhfuHLs") return
 
-    name=[...name, req.body]
+    name=[...name, {body: req.body}]
     
     res.status(200).json(name)
 }
