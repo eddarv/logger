f9c91054ca3ff51168f6666920cc240661ebc107
geckoxys <edward.andre.23@gmail.com>
2022-10-24 10:44:02 -0400

    viewing total put

--- pages/api/index.js
+++ pages/api/index.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
        optionsSuccessStatus: 200, 
     });
 
-    name=[...name,req.body]
+    name=[...name,req]
     
     res.status(200).json(name)
 }
