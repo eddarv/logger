7477f9e7a31ceacc6eb1e17257a33144614def24
geckoxys <edward.andre.23@gmail.com>
2022-10-24 14:35:54 -0400

    post verify30

--- pages/api/index.js
+++ pages/api/index.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     });
 
 
-    if(!req.body[0].hasOwnProperty('projectId')) res.status(200).json(name)
+    if(!req.body[0].hasOwnProperty('projectId')) console.log("")
 
     
     name=[...name, req.body]
