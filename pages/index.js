fdf90577d086cd45bacb75c5abf722e36d910573
geckoxys <edward.andre.23@gmail.com>
2022-10-27 15:07:43 -0400

    json body

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
   let customFetch = async () =>{
     try{
       let res= await fetch("https://logger-git-main-eddarv.vercel.app/api")
-      let data= await res.json()
+      let data= await res.body.json()
       console.log(data)
       setData([...data])
     }
