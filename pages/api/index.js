c6261211ae6b81af3c35113547a916899a6200ef
nazhG <nazhirgonzalez@gmail.com>
2022-11-02 15:08:38 -0400

    res handler

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,84 +1,73 @@
-import NextCors from 'nextjs-cors'
-import {PrismaClient} from '@prisma/client'
-const prisma = new PrismaClient()
+import NextCors from "nextjs-cors";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-let name=[]
-let auxObj={projectId: false}
+let name = [];
+let auxObj = { projectId: false };
 
 async function save(log) {
-    const post = await prisma.log.create({
-        data:{
-            logId: log.id,
-            message: log.message,
-            timestamp: log.timestamp,
-            type: log.type,
-            source:log.source,
-            host:log.host,
-            entrypoint:log.entrypoint
-        }
-    })
+  const post = await prisma.log.create({
+    data: {
+      logId: log.id,
+      message: log.message,
+      timestamp: log.timestamp,
+      type: log.type,
+      source: log.source,
+      host: log.host,
+      entrypoint: log.entrypoint,
+    },
+  });
 }
 
 async function read(log) {
-    const post = await prisma.log.findMany()
-    return post
+  const post = await prisma.log.findMany();
+  return post;
 }
 
 export default async function handler(req, res) {
+  try {
+    await NextCors(req, res, {
+      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+      origin: "*",
+      optionsSuccessStatus: 200,
+    });
 
-    try{
-        await NextCors(req, res, {
+    if (req.method === "GET") {
+      name = await read()
+        .catch((e) => {
+          return res.status(500).send("error en respuestas");
+        })
+        .finally(async () => {
+          await prisma.$disconnect();
+        });
 
-            methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-            origin: '*',
-            optionsSuccessStatus: 200, 
-         });
-     
-     
-        if(req.method==='GET') {
-            name = await read()
-                .catch(e=> {
-                    return res.status(500).send("error en respuestas")
-                })
-                .finally(async ()=>{
-                    await prisma.$disconnect()
-                })
-            
-            if(!name.ok) return res.status(500).send("res not ok")
+      console.log({ name });
 
-            if(name.length===0) return res.status(200).json([1])
-            return res.status(200).send("res ok")
-        }
-
-        if(req.method==='POST') {
-            auxObj={...req.body[0]}
-     
-            if (auxObj.projectId!=="prj_Uneqt4CLqAh6YprTsAUfBqhfuHLs") return
-
-            await save(req.body[0])
-                .catch(e=> {
-                    console.log(e.message)
-                })
-                .finally(async ()=>{
-                    await prisma.$disconnect()
-                })
-            
-            name=[...name, req.body[0]]
-            
-            res.status(200).json(name)
-        }
-
-        return
-     
-         
+      if (name.length === 0) return res.status(200).json([1]);
+      return res.status(200).send("res ok");
     }
 
-    catch(e){
-        console.log(e)
-        res.status(400).json(e)
+    if (req.method === "POST") {
+      auxObj = { ...req.body[0] };
+
+      if (auxObj.projectId !== "prj_Uneqt4CLqAh6YprTsAUfBqhfuHLs") return;
+
+      await save(req.body[0])
+        .catch((e) => {
+          console.log(e.message);
+        })
+        .finally(async () => {
+          await prisma.$disconnect();
+        });
+
+      name = [...name, req.body[0]];
+
+      res.status(200).json(name);
     }
 
-    
-    
-    
+    return;
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
 }
